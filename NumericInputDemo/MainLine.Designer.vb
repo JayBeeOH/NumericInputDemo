c48134bb0b528e0bfc3bd82b2768d5af3849b2b1
Joseph Bolen <bolenonline@roadrunner.com>
2017-06-10 15:49:44 -0400

    Add project files.

---
+++ NumericInputDemo/MainLine.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainLine
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainLine))
+        Me.StandardTBLabel = New System.Windows.Forms.Label()
+        Me.StandardTextBox = New System.Windows.Forms.TextBox()
+        Me.KeyPressTextBox = New System.Windows.Forms.TextBox()
+        Me.KeyPressTBLabel = New System.Windows.Forms.Label()
+        Me.ValidatingTextBox = New System.Windows.Forms.TextBox()
+        Me.ValidatingTBLabel = New System.Windows.Forms.Label()
+        Me.SubmitButton = New System.Windows.Forms.Button()
+        Me.MyStatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.StatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.MyStatusStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StandardTBLabel
+        '
+        Me.StandardTBLabel.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.StandardTBLabel.AutoSize = True
+        Me.StandardTBLabel.Location = New System.Drawing.Point(41, 37)
+        Me.StandardTBLabel.Name = "StandardTBLabel"
+        Me.StandardTBLabel.Size = New System.Drawing.Size(100, 15)
+        Me.StandardTBLabel.TabIndex = 0
+        Me.StandardTBLabel.Text = "Standard TextBox:"
+        '
+        'StandardTextBox
+        '
+        Me.StandardTextBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.StandardTextBox.Location = New System.Drawing.Point(157, 34)
+        Me.StandardTextBox.Name = "StandardTextBox"
+        Me.StandardTextBox.Size = New System.Drawing.Size(100, 23)
+        Me.StandardTextBox.TabIndex = 1
+        '
+        'KeyPressTextBox
+        '
+        Me.KeyPressTextBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.KeyPressTextBox.Location = New System.Drawing.Point(157, 78)
+        Me.KeyPressTextBox.Name = "KeyPressTextBox"
+        Me.KeyPressTextBox.Size = New System.Drawing.Size(100, 23)
+        Me.KeyPressTextBox.TabIndex = 3
+        '
+        'KeyPressTBLabel
+        '
+        Me.KeyPressTBLabel.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.KeyPressTBLabel.AutoSize = True
+        Me.KeyPressTBLabel.Location = New System.Drawing.Point(41, 81)
+        Me.KeyPressTBLabel.Name = "KeyPressTBLabel"
+        Me.KeyPressTBLabel.Size = New System.Drawing.Size(102, 15)
+        Me.KeyPressTBLabel.TabIndex = 2
+        Me.KeyPressTBLabel.Text = "Key Press TextBox:"
+        '
+        'ValidatingTextBox
+        '
+        Me.ValidatingTextBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ValidatingTextBox.Location = New System.Drawing.Point(157, 122)
+        Me.ValidatingTextBox.Name = "ValidatingTextBox"
+        Me.ValidatingTextBox.Size = New System.Drawing.Size(100, 23)
+        Me.ValidatingTextBox.TabIndex = 5
+        '
+        'ValidatingTBLabel
+        '
+        Me.ValidatingTBLabel.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ValidatingTBLabel.AutoSize = True
+        Me.ValidatingTBLabel.Location = New System.Drawing.Point(41, 125)
+        Me.ValidatingTBLabel.Name = "ValidatingTBLabel"
+        Me.ValidatingTBLabel.Size = New System.Drawing.Size(105, 15)
+        Me.ValidatingTBLabel.TabIndex = 4
+        Me.ValidatingTBLabel.Text = "Validating TextBox:"
+        '
+        'SubmitButton
+        '
+        Me.SubmitButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SubmitButton.Location = New System.Drawing.Point(244, 173)
+        Me.SubmitButton.Name = "SubmitButton"
+        Me.SubmitButton.Size = New System.Drawing.Size(75, 23)
+        Me.SubmitButton.TabIndex = 6
+        Me.SubmitButton.Text = "&Submit"
+        Me.SubmitButton.UseVisualStyleBackColor = True
+        '
+        'MyStatusStrip
+        '
+        Me.MyStatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StatusLabel})
+        Me.MyStatusStrip.Location = New System.Drawing.Point(0, 209)
+        Me.MyStatusStrip.Name = "MyStatusStrip"
+        Me.MyStatusStrip.Size = New System.Drawing.Size(331, 22)
+        Me.MyStatusStrip.TabIndex = 7
+        Me.MyStatusStrip.Text = "StatusStrip1"
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(0, 17)
+        '
+        'MainLine
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.ClientSize = New System.Drawing.Size(331, 231)
+        Me.Controls.Add(Me.MyStatusStrip)
+        Me.Controls.Add(Me.SubmitButton)
+        Me.Controls.Add(Me.ValidatingTextBox)
+        Me.Controls.Add(Me.ValidatingTBLabel)
+        Me.Controls.Add(Me.KeyPressTextBox)
+        Me.Controls.Add(Me.KeyPressTBLabel)
+        Me.Controls.Add(Me.StandardTextBox)
+        Me.Controls.Add(Me.StandardTBLabel)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "MainLine"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Numeric Input Demo"
+        Me.MyStatusStrip.ResumeLayout(False)
+        Me.MyStatusStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StandardTBLabel As Label
+    Friend WithEvents StandardTextBox As TextBox
+    Friend WithEvents KeyPressTextBox As TextBox
+    Friend WithEvents KeyPressTBLabel As Label
+    Friend WithEvents ValidatingTextBox As TextBox
+    Friend WithEvents ValidatingTBLabel As Label
+    Friend WithEvents SubmitButton As Button
+    Friend WithEvents MyStatusStrip As StatusStrip
+    Friend WithEvents StatusLabel As ToolStripStatusLabel
+End Class
---
+++ NumericInputDemo/MainLine.vb
@@ -0,0 +1,136 @@
+﻿'------------------------------------------------------------------------------------------
+'           Notice of My Copyright and Intellectual Property Rights
+'
+'            Copyright © 2017 Joseph L. Bolen. All rights reserved.
+'        All trademarks remain the property of their respective owners.
+'
+'    This program Is free software: you can redistribute it And/Or modify
+'    it under the terms Of the GNU General Public License As published by
+'    the Free Software Foundation, either version 3 Of the License, Or
+'    any later version.
+'
+'    This program Is distributed In the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty Of
+'    MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License For more details.
+'
+'    You should have received a copy Of the GNU General Public License
+'    along with this program.  If Not, see < http: //www.gnu.org/licenses/>.
+'
+'-------------------------------------------------------------------------------------------
+' Program Name:   Numeric Input Demo
+' Class:          MainLine
+'
+' Author:         Joseph L. Bolen
+' Date Created:   10 JUN 2017
+'
+' Description:    Demo using Textboxes for numeric input and the ErrorProvider.
+'
+'                 Documentation is at:
+'                   App's documentation is at:  https://jaybeeoh.github.io/NumericInputDemo/
+'                   Video tutorials at YouTube: http://www.youtube.com/user/bolenpresents
+'-------------------------------------------------------------------------------------------
+
+Imports System.ComponentModel
+
+Public Class MainLine
+
+    Private WithEvents MyErrorProvider As New ErrorProvider With {.BlinkStyle = ErrorBlinkStyle.NeverBlink}
+
+
+    Private Sub MainLine_Load(sender As Object, e As EventArgs) _
+        Handles MyBase.Load
+
+        ' Allow tabbing when using ErrorProvider
+        AutoValidate = Windows.Forms.AutoValidate.EnableAllowFocusChange
+    End Sub
+
+    ' Allow only Carriage Return (13), BackSpace(8) and Period(46) and Numbers.
+    Private Sub KeyPressTextBox_KeyPress(sender As Object, e As KeyPressEventArgs) _
+        Handles KeyPressTextBox.KeyPress
+
+        Dim ctrl As Control = CType(sender, Control)
+        MyErrorProvider.SetError(ctrl, String.Empty)
+        If Asc(e.KeyChar) <> 13 AndAlso
+            Asc(e.KeyChar) <> 8 AndAlso
+            Asc(e.KeyChar) <> 46 AndAlso
+            Not IsNumeric(e.KeyChar) Then
+            MyErrorProvider.SetError(ctrl, "Please enter numbers only.")
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub ValidatingTextBox_Validating(sender As Object, e As CancelEventArgs) _
+        Handles ValidatingTextBox.Validating
+
+        Dim ctrl As Control = CType(sender, Control)
+        MyErrorProvider.SetError(ctrl, String.Empty)
+        Try
+            ValidateIsRequired(ctrl.Text)
+            ValidateIsNumeric(ctrl.Text)
+            ValidateInRange(ctrl.Text, 0, 100)
+        Catch ex As Exception
+            MyErrorProvider.SetError(ctrl, ex.Message)
+            e.Cancel = True
+        End Try
+    End Sub
+
+
+    Private Sub SubmitButton_Click(sender As Object, e As EventArgs) _
+        Handles SubmitButton.Click
+
+        StatusLabel.Text = String.Empty
+
+        If Not ValidateChildren() Then
+            StatusLabel.ForeColor = Color.Red
+            StatusLabel.Text = "Please correct entered data."
+            My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Hand)
+            Exit Sub
+        End If
+
+        StatusLabel.ForeColor = Color.Green
+        StatusLabel.Text = "Data is has been validated."
+    End Sub
+
+    Private Sub MainLine_FormClosing(sender As Object, e As FormClosingEventArgs) _
+        Handles Me.FormClosing
+
+        e.Cancel = False    ' Allow close with errors pending using ErrorProvider.
+    End Sub
+
+    ' Normally, these and other Validation functions as placed in their own module.
+    Public Function ValidateIsRequired(ByVal value As String) As Boolean
+
+        If String.IsNullOrWhiteSpace(value) Then
+            Throw New InvalidExpressionException("A value is required.")
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Public Function ValidateIsNumeric(ByVal value As String) As Boolean
+
+        If Not IsNumeric(value) Then
+            Throw New InvalidExpressionException("Value must be numeric.")
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Public Function ValidateInRange(ByVal value As String,
+                                  ByVal minValue As Double,
+                                  ByVal maxValue As Double) As Boolean
+
+        Dim valueDouble As Double = 0
+        Double.TryParse(value, valueDouble)
+        If valueDouble < minValue OrElse valueDouble > maxValue Then
+            Throw New InvalidExpressionException(String.Format("Value is out of range, please enter a number between {0} and {1}.", minValue, maxValue))
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+End Class
